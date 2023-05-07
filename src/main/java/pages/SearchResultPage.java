0a2b826d2ed1491e8491fea746e948a5a103b3f8
wegor84 <132587798+wegor84@users.noreply.github.com>
2023-05-08 00:47:08 +0200

    use my another metod to wait for element         if (waitTo3sek(pomoc)) {             imagesLink.click();

--- src/main/java/pages/SearchResultPage.java
+++ src/main/java/pages/SearchResultPage.java
@@ -2,11 +2,11 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactoryFinder;
+
 
 import java.util.List;
 
-public class SearchResultPage {
+public class SearchResultPage extends PageBase{
     @FindBy(linkText = "Grafika")
     WebElement imagesLink;
 
@@ -16,12 +16,16 @@ public class SearchResultPage {
 
     public void goToImages() throws InterruptedException {
 
-        for ( int i =30; i > 0;i--){
-            System.out.println("i wynosi " +i);
-            Thread.sleep(100);
-            if (pomoc.size() == 1) i = 0;
+//        for ( int i =30; i > 0;i--){
+//            System.out.println("i wynosi " +i);
+//            Thread.sleep(100);
+//            if (pomoc.size() == 1) i = 0;
+//        }
+//        imagesLink.click();
+
+        if (waitTo3sek(pomoc)) {
+            imagesLink.click();
         }
-        imagesLink.click();
 
     }
 }
